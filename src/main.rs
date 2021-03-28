202daa8aac8532301f910ae348fb8c36a8139a02
Rafael Ferrés <rafael.ferres-jum@parceiros-kroton.com.br>
2021-03-28 17:26:03 -0300

    Hello World result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use rocket::*;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/api/v1", routes![index]).launch();
 }
